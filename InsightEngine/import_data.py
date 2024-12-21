03bef993d84e37e249baa7c32d00ec7706c53f3e
Jeevanandh007 <jeevavijay24@gmail.com>
2024-12-21 00:02:19 +0000

    Metabase dashboard update

--- InsightEngine/import_data.py
+++ InsightEngine/import_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import psycopg2
 from datetime import datetime
 
-df = pd.read_csv('machine_data.csv')
+df = pd.read_csv('machine_data_9000.csv')
 
 conn = psycopg2.connect(
     dbname ="insightengine",
@@ -12,27 +12,34 @@ conn = psycopg2.connect(
 
 )
 
+
+print("Available columns:", df.columns.tolist())
+print("\n first few rows: ")
+print(df.head())
+        
+
 cur = conn.cursor()
 
 for index, row in df.iterrows():
         cur.execute("""
             INSERT INTO machine_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (
-            datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S'),
-            row['udi'],
-            row['type'],
-            row['air_temperature'],
-            row['process_temperature'],
-            row['rotational_speed'],
-            row['torque'],
-            row['tool_wear'],
-            row['machine_failure'],
-            row['twf'],
-            row['hdf'],
-            row['pwf'],
-            row['osf'],
-            row['rnf']
+            datetime.strptime(row['Timestamp'], '%d-%m-%y %H:%M'),
+            row['UID'],
+            row['Type'],
+            row['Air temperature [K]'],
+            row['Process temperature [K]'],
+            row['Rotational speed [rpm]'],
+            row['Torque [Nm]'],
+            row['Tool wear [min]'],
+            row['Machine failure'],
+            row['TWF'],
+            row['HDF'],
+            row['PWF'],
+            row['OSF'],
+            row['RNF']
         ))
 conn.commit()
 cur.close()
 conn.close()
+ 
