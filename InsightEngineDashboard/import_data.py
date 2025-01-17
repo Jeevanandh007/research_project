071f09ebe754905535c472668b0c6418138032c0
Jeevanandh007 <jeevavijay24@gmail.com>
2025-01-17 12:55:46 +0000

    insert old data update

--- InsightEngineDashboard/import_data.py
+++ InsightEngineDashboard/import_data.py
@@ -1,45 +1,64 @@
-import pandas as pd 
+import pandas as pd
 import psycopg2
 from datetime import datetime
 
+
 df = pd.read_csv('machine_data_9000.csv')
 
+# Database connection
 conn = psycopg2.connect(
-    dbname ="insightengine",
-    user ="postgres",
-    password ="12345",
-    host ="172.27.226.187"
-
+    dbname="insightengine",
+    user="postgres",
+    password="postgres",
+    host="postgres"
 )
 
-
 print("Available columns:", df.columns.tolist())
-print("\n first few rows: ")
+print("\nFirst few rows:")
 print(df.head())
-        
 
 cur = conn.cursor()
 
+# Insert data 
 for index, row in df.iterrows():
-        cur.execute("""
-            INSERT INTO machine_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (
-            datetime.strptime(row['Timestamp'], '%d-%m-%y %H:%M'),
-            row['UID'],
-            row['Type'],
-            row['Air temperature [K]'],
-            row['Process temperature [K]'],
-            row['Rotational speed [rpm]'],
-            row['Torque [Nm]'],
-            row['Tool wear [min]'],
-            row['Machine failure'],
-            row['TWF'],
-            row['HDF'],
-            row['PWF'],
-            row['OSF'],
-            row['RNF']
-        ))
+    cur.execute("""
+        INSERT INTO machine_data
+                (
+            "timestamp",
+            air_temperature,
+            process_temperature,
+            rotational_speed,
+            torque,
+            tool_wear,
+            machine_status,
+            prediction_status,
+            product_id,
+            type,
+            twf,
+            hdf,
+            pwf,
+            osf,
+            rnf)
+                VALUES 
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        datetime.strptime(row['Timestamp'], '%d-%m-%y %H:%M'),
+        row['Air temperature [K]'],
+        row['Process temperature [K]'],
+        row['Rotational speed [rpm]'],
+        row['Torque [Nm]'],
+        row['Tool wear [min]'],
+        row['Machine failure'],
+        row['Machine failure'],
+        row['Product ID'],
+        row['Type'],
+        row['TWF'],
+        row['HDF'],
+        row['PWF'],
+        row['OSF'],
+        row['RNF']
+    ))
+
 conn.commit()
 cur.close()
 conn.close()
- 
